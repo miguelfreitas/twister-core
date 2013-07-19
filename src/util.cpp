1556e436c522774dc714fbb461daf6d1b03480c7
Miguel Freitas <miguel@pitanga.cpti.cetuc.puc-rio.br>
2013-07-19 19:32:57 -0300

    fix -datadir inicialization and debug log

--- src/util.cpp
+++ src/util.cpp
@@ -526,6 +526,8 @@ static void InterpretNegativeSetting(string name, map<string, string>& mapSettin
 
 void ParseParameters(int argc, const char* const argv[])
 {
+    fCachedPath[0] = fCachedPath[1] = false; // clear cached path to receive cmdline
+
     mapArgs.clear();
     mapMultiArgs.clear();
     for (int i = 1; i < argc; i++)
@@ -567,6 +569,8 @@ void ParseParameters(int argc, const char* const argv[])
         // interpret -nofoo as -foo=0 (and -nofoo=0 as -foo=1) as long as -foo not set
         InterpretNegativeSetting(name, mapArgs);
     }
+
+    fReopenDebugLog = true;
 }
 
 std::string GetArg(const std::string& strArg, const std::string& strDefault)
