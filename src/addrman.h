274ffeb1f6a2ffe6b83e455af9612d90e054c266
Miguel Freitas <miguel@mf.site>
2013-10-13 16:57:35 -0300

    increase percentage of getaddr (ADDRMAN_GETADDR_MAX_PCT) until we grow older and bigger

--- src/addrman.h
+++ src/addrman.h
@@ -156,7 +156,8 @@ public:
 #define ADDRMAN_MIN_FAIL_DAYS 7
 
 // the maximum percentage of nodes to return in a getaddr call
-#define ADDRMAN_GETADDR_MAX_PCT 23
+//#define ADDRMAN_GETADDR_MAX_PCT 23
+#define ADDRMAN_GETADDR_MAX_PCT 100 // [MF]
 
 // the maximum number of nodes to return in a getaddr call
 #define ADDRMAN_GETADDR_MAX 2500
