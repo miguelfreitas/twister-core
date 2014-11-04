dd9c05e918bcc5c3f115125ac7e20e19963a7f1f
Miguel Freitas <miguelfreitas@users.noreply.github.com>
2014-11-04 17:24:14 -0200

    updating softcheckpoint voters

--- src/softcheckpoint.cpp
+++ src/softcheckpoint.cpp
@@ -37,11 +37,11 @@ namespace SoftCheckpoints
     static std::set<std::string> uniqueUsersList =
             boost::assign::list_of
             ("mf1")("mf1a")("mf2")("mf2a")("mf3")
-            ("quietpatron")("wn41");
+            ("wn41");
             
     static std::set<std::string> upcomingUsersList =
             boost::assign::list_of
-            ("_gltest2")("fcplm_zion");
+            ("omgasm");
 
 
     void SetSoftCPBestChain() {
