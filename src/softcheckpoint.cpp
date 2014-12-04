0f5ea415a2089b4e37779185aab6194a4479837b
Miguel Freitas <miguelfreitas@users.noreply.github.com>
2014-12-04 17:25:18 -0200

    params update

--- src/softcheckpoint.cpp
+++ src/softcheckpoint.cpp
@@ -37,11 +37,11 @@ namespace SoftCheckpoints
     static std::set<std::string> uniqueUsersList =
             boost::assign::list_of
             ("mf1")("mf1a")("mf2")("mf2a")("mf3")
-            ("wn41");
+            ("wn41")("omgasm")("rantizdani")("rantisluni");
             
     static std::set<std::string> upcomingUsersList =
             boost::assign::list_of
-            ("omgasm")("rantizdani")("rantisluni");
+            ("nobody");
 
 
     void SetSoftCPBestChain() {
