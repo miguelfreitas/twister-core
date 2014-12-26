d47fa93bb96314633a6d5da8801c78e54a468a29
Miguel Freitas <miguelfreitas@users.noreply.github.com>
2014-12-26 20:01:33 -0200

    android build fixes

--- libtorrent/runme-android.sh
+++ libtorrent/runme-android.sh
@@ -1,10 +1,13 @@
-export PATH=/home/miguel/android/android-ndk-r8b/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86/bin/:$PATH
-export NDK=/home/miguel/android/android-ndk-r8b
+export PATH=$HOME/android/android-ndk-r8b/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86/bin/:$PATH
+export NDK=$HOME/android/android-ndk-r8b
 export NDK_BASE=$NDK
 export SYSROOT=$NDK/platforms/android-9/arch-arm
-export BOOSTDIR=/home/miguel/android/Boost-for-Android/build/
+export BOOSTDIR=$HOME/android/Boost-for-Android/build/
 
 if [ $1 == "configure" ]; then
+if [ ! -f configure ]; then
+  ./bootstrap.sh
+fi
 ./configure --host=arm-linux-androideabi --enable-shared=no --enable-static=yes \
  --enable-debug \
  CPPFLAGS="-I$SYSROOT/usr/include/ -I$BOOSTDIR/include \
@@ -14,7 +17,7 @@ if [ $1 == "configure" ]; then
  LDFLAGS="-Wl,-rpath-link=$SYSROOT/usr/lib/ -L$SYSROOT/usr/lib/ -L$BOOSTDIR/lib \
  $BOOSTDIR/lib/libboost_system-gcc-mt-1_49.a $BOOSTDIR/lib/libboost_thread-gcc-mt-1_49.a \
  $NDK_BASE/sources/cxx-stl/gnu-libstdc++/4.6/libs/armeabi/libgnustl_static.a" \
- LIBS="-lc" CFLAGS="-nostdlib" --prefix="$SYSROOT/usr" \
+ LIBS="-lc -lssl -lcrypto" CFLAGS="-nostdlib" --prefix="$SYSROOT/usr" \
  --with-boost=$BOOSTDIR --with-boost-libdir=$BOOSTDIR/lib  && make -j2
 fi
 
