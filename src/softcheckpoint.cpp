d09d5f58be40c41d6836ea6202dc014b577934c7
Miguel Freitas <miguelfreitas@users.noreply.github.com>
2017-02-07 11:27:22 -0200

    updating soft checkpoints

--- src/softcheckpoint.cpp
+++ src/softcheckpoint.cpp
@@ -41,7 +41,7 @@ namespace SoftCheckpoints
             
     static std::set<std::string> upcomingUsersList =
             boost::assign::list_of
-            ("nobody")("mf2a");
+            ("nobody")("mf2a")("lxoliva")("stman");
 
 
     void SetSoftCPBestChain() {
